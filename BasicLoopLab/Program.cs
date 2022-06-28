f2e25f956dafdf29b366fce5982e5eccb31fc978
MacobJagyar <jacobmagyar95@gmail.com>
2022-06-28 08:07:04 -0400

    Final commit.

--- BasicLoopLab/Program.cs
+++ BasicLoopLab/Program.cs
@@ -49,10 +49,57 @@ if (answer == "n")
 
 ///////////////End of exercise 2
 
+/*
+string answer = " ";
+
+while (answer != "13579")
+{
+    Console.WriteLine("Hello! Please enter the correct door code: ");
+    answer = (Console.ReadLine());
+    if (answer != "13579")
+    {
+        Console.WriteLine("Wrong code!\n");
+    } else
+    {
+        Console.WriteLine("Welcome home!");
+    }
+}
+*/
 
 
+///////////////End of exercise 3
 
 
+/*
+string answer = " ";
+int attempts = 0;
+
+while (answer != "13579")
+{
+    Console.WriteLine("Hello! Please enter the correct door code: ");
+    answer = (Console.ReadLine());
+    if (answer != "13579")
+    {
+        Console.WriteLine("Wrong code!\n");
+        attempts++;
+    }
+    else
+    {
+        Console.WriteLine("Welcome home!");
+    }
+
+    if (attempts == 5)
+    {
+        Console.WriteLine("WARNING: Maximum attempts exceeded.");
+        break;
+    }
+}
+*/
+
+///////////////End of exercise 4
+
+
+///////////////See the Exercise 5 project
 
 
 
---
+++ Exercise 5/Program.cs
@@ -0,0 +1,29 @@
+﻿string answer = " ";
+int attempts = 0;
+string password = "13579";
+do
+{
+    Console.WriteLine("Hello! Please enter the correct door code: ");
+    answer = (Console.ReadLine());
+
+
+    if (answer != password)
+    {
+        Console.WriteLine("Wrong Code! Please try again\n");
+        attempts++;
+    }
+
+    if (attempts == 5)
+    {
+        Console.WriteLine("\nWrong code!\nWARNING: Maximum attempts exceeded.");
+        break;
+    }
+
+    if (answer == password)
+    {
+        Console.WriteLine("Welcome Home!");
+    }
+
+} while (answer != password);
+
+Console.ReadKey();
