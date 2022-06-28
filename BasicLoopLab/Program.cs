fbb76e3203a7d7cf88f29ae399c7dc34b80b071e
MacobJagyar <jacobmagyar95@gmail.com>
2022-06-27 21:41:31 -0400

    Third Commit

--- BasicLoopLab/Program.cs
+++ BasicLoopLab/Program.cs
@@ -1,1 +1,65 @@
-﻿Console.ReadLine();
+﻿/* string answer = "";
+
+do
+{
+    Console.WriteLine("Hello World!");
+    Console.Write("Would you like to continue? (y/n) ");
+    answer = Console.ReadLine();
+
+} while (answer == "y"); */
+
+
+///////////////End of exercise 1
+
+
+string answer = " ";
+
+do
+{
+    Console.Write("Enter a number: ");
+    int input = int.Parse(Console.ReadLine());
+
+    for (int i = input; i >= 0; i--)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine(" ");
+
+    for (int i = 0; i <= input; i++)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine(" ");
+
+    Console.WriteLine("Would you like to continue? (y/n)");
+    answer = Console.ReadLine();
+} while (answer == "y"); 
+
+if (answer == "n")
+{
+    Console.WriteLine("Goodbye!");
+}
+
+
+///////////////End of exercise 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
